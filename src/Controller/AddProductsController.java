7b2e1e2a7ff899a759ecdfe1cbbd96d1862058ba
Caleb ONeill <oneill479@gmail.com>
2021-07-21 21:08:00 -0500

    added in table ids and functions

--- src/Controller/AddProductsController.java
+++ src/Controller/AddProductsController.java
@@ -1,11 +1,17 @@
 package Controller;
 
+import Model.Part;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,9 +20,32 @@ import java.util.ResourceBundle;
 
 public class AddProductsController implements Initializable {
 
+    public TableView productAddPartTable;
+    public TableColumn productAddIdColumn;
+    public TableColumn productAddNameColumn;
+    public TableColumn productAddInventoryColumn;
+    public TableColumn productAddPriceColumn;
+
+    public TableView productRemovePartTable;
+    public TableColumn productRemoveIdColumn;
+    public TableColumn productRemoveNameColumn;
+    public TableColumn productRemoveInventoryColumn;
+    public TableColumn productRemovePriceColumn;
+
+    public TextField addProductId;
+    public TextField addProductName;
+    public TextField addProductInventory;
+    public TextField addProductPrice;
+    public TextField addProductMax;
+    public TextField addProductMin;
+
+    private ObservableList<Part> addPart = FXCollections.observableArrayList();
+    private ObservableList<Part> removePart = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        productAddPartTable.setItems(addPart);
     }
 
     public void toMain(ActionEvent actionEvent) throws IOException {
--- src/Controller/MainScreenController.java
+++ src/Controller/MainScreenController.java
@@ -1,12 +1,19 @@
 package Controller;
 
+import Model.Part;
+import Model.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,14 +22,50 @@ import java.util.ResourceBundle;
 
 public class MainScreenController implements Initializable {
 
+    public TableView partTable;
+    public TableColumn partIdColumn;
+    public TableColumn partNameColumn;
+    public TableColumn partInventoryColumn;
+    public TableColumn partPriceColumn;
+
+    public TableView productTable;
+    public TableColumn productIdColumn;
+    public TableColumn productNameColumn;
+    public TableColumn productInventoryColumn;
+    public TableColumn productPriceColumn;
+
+    public TextField partSearch;
+    public TextField productSearch;
+
+    private ObservableList<Part> mainParts = FXCollections.observableArrayList();
+    private ObservableList<Product> mainProducts = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        partTable.setItems(mainParts);
+        partIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        partNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        partInventoryColumn.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        partPriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        productTable.setItems(mainProducts);
+        productIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        productNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        productInventoryColumn.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        productPriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+
+        mainParts.add(new Part(1, "Wheel", 7.99, 4, 2, 8) {
+
+        });
+
+        mainProducts.add(new Product(1, "Bike", 59.99, 3, 2, 4));
     }
 
     public void toAddParts(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/View/AddParts.fxml"));
-        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setTitle("Add Parts");
         stage.setScene(scene);
@@ -31,7 +74,7 @@ public class MainScreenController implements Initializable {
 
     public void toModifyParts(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/View/ModifyParts.fxml"));
-        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setTitle("Modify Parts");
         stage.setScene(scene);
@@ -40,7 +83,7 @@ public class MainScreenController implements Initializable {
 
     public void toAddProducts(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/View/AddProducts.fxml"));
-        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setTitle("Add Products");
         stage.setScene(scene);
@@ -49,11 +92,24 @@ public class MainScreenController implements Initializable {
 
     public void toModifyProducts(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/View/ModifyProducts.fxml"));
-        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setTitle("Modify Products");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void deletePart(ActionEvent actionEvent) {
+        Part selectedPart = (Part) partTable.getSelectionModel().getSelectedItem();
+
+        if(selectedPart == null) return;
+        mainParts.remove(selectedPart);
+    }
+
+    public void deleteProduct(ActionEvent actionEvent) {
+        Product selectedProduct = (Product) productTable.getSelectionModel().getSelectedItem();
+        if(selectedProduct == null) return;
+        mainProducts.remove(selectedProduct);
     }
 
     public void exit() {
